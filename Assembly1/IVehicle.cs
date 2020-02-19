3a80818cb13dd5686318a8c4980d9ea7ef604bf5
Joachim Rautelius <Rautan_97@live.com>
2020-02-19 13:29:54 +0100

    aah

---
+++ Assembly1/IVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly1
+{
+    public interface IVehicle
+    {
+        Enum Type { get; }
+        DateTime Arrival { get; }
+        string RegNum { get; }
+        decimal Getcost { get; }
+
+        string ToString();
+    }
+}
