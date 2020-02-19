8b2c1c716a31344c17860c47822d76c2fb5787fe
Scorpie <rautan_97@live.com>
2020-02-19 13:49:20 +0100

    que pasa
    Commit this yall

---
+++ Assembly2/ParkingLot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly2
+{
+    public class ParkingLot
+    {
+        private List<Spot>
+    }
+}
---
+++ Assembly2/Spot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly2
+{
+    internal class Spot
+    {
+        private int capacity;
+        internal Spot(int capacity)
+        {
+            this.capacity = capacity;
+        }
+    }
+}
