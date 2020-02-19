9836db6bdcef3b8c04b4bd6833495922e7826547
HoodDoggeh <50180470+HoodDoggeh@users.noreply.github.com>
2020-02-19 14:06:44 +0100

    Added dependencies

--- Assembly2/Spot.cs
+++ Assembly2/Spot.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assembly1;
 
 namespace Assembly2
 {
     internal class Spot
     {
         private int capacity;
+
+        internal Spot()
+        {
+        }
         internal Spot(int capacity)
         {
             this.capacity = capacity;
+        }
+
+        bool AddVehicle(IVehicle vehicle)
+        {
+            false;
+        }
+        
+        IVehicle RemoveVehicle(string regNum)
+        {
+            
         }
     }
 }
