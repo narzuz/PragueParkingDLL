bf79fc9b58d8edc727603d605fc09a09b71423e2
Scorpie <Rautan_97@live.com>
2020-02-25 11:33:49 +0100

    testing here yo
    dth

---
+++ ASSEMBLYREEMSTESTS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ASSEMBLYREEMSTESTS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASSEMBLYREEMSTESTS")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dcee0e56-b382-47f3-a71d-2f91a748d6e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ASSEMBLYREEMSTESTS/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assembly1;
+using Assembly2;
+
+namespace ASSEMBLYREEMSTESTS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataRow("REEE", Assembly1.Type.Car, 30)]
+        [DataRow("EREE", Assembly1.Type.Car, 1)]
+        [DataRow("EERE", Assembly1.Type.Mc, 60)]
+        [DataTestMethod]
+        public void TestParkingAtSpot(string regNum, Assembly1.Type type, int sputter)
+        {
+            ParkingLot lot = new ParkingLot();
+            IVehicle vehicle = new Vehicle(regNum, type);
+            Assert.IsTrue(lot.Park(vehicle, sputter));
+        }
+
+        [DataRow("REEE", Assembly1.Type.Car, 50)]
+        [DataRow("EREE", Assembly1.Type.Car, 49)]
+        [DataRow("EERE", Assembly1.Type.Mc, 32)]
+        [DataTestMethod]
+        public void TestFindVehicle(string regNum, Assembly1.Type type, int spot)
+        {
+            ParkingLot lot = new ParkingLot();
+            IVehicle vehicle = new Vehicle(regNum, type);
+
+            lot.Park(vehicle, spot);
+            int expected = spot;
+            int actual = lot.FindVehicle(regNum);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [DataRow("REEE", Assembly1.Type.Car, 15)]
+        [DataRow("EREE", Assembly1.Type.Car, 33)]
+        [DataRow("EERE", Assembly1.Type.Mc, 60)]
+        [DataTestMethod]
+        public void TestMoveVehicle(string regNum, Assembly1.Type type, int sputter)
+        {
+            ParkingLot lot = new ParkingLot();
+            IVehicle vehicle = new Vehicle(regNum, type);
+
+            lot.Park(vehicle);
+
+            Assert.IsTrue(lot.MoveVehicle(regNum, sputter));
+
+        }
+
+
+    }
+}
